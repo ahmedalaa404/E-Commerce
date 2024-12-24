43134427814902a1783f9eebd96bdbabe9796bb1
ahmedalaa404 <az.ahmed.alaa@gmail.com>
2024-12-24 23:45:56 +0200

    Upload Project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { CartComponent } from './Component/cart/cart.component';
+import { LoginComponent } from './Component/login/login.component';
+import { RegisterComponent } from './Component/register/register.component';
+import { BrandsComponent } from './Component/brands/brands.component';
+import { ProductsComponent } from './Component/products/products.component';
+import { CategoriesComponent } from './Component/categories/categories.component';
+import { NotfoundComponent } from './Component/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:" ",redirectTo:"/home",pathMatch:"full"},
+    {path:"home",component:HomeComponent},
+    {path:"cart",component:CartComponent},
+    {path:"login",component:LoginComponent},
+    {path:"register",component:RegisterComponent},
+    {path:"Brands",component:BrandsComponent},
+    {path:"Product",component:ProductsComponent },
+    {path:"categories",component:CategoriesComponent},
+    {path:"**",component:NotfoundComponent},
+];
