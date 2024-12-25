93fc535d801ccac8b140a77814d19025babf5de7
ahmedalaa404 <az.ahmed.alaa@gmail.com>
2024-12-25 20:32:41 +0200

    Make Serices to make Authentication

--- src/app/Component/register/register.component.ts
+++ src/app/Component/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import {FormControl, ReactiveFormsModule,FormGroup, Validators} from '@angular/forms'
+import {FormControl, ReactiveFormsModule,FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms'
 @Component({
   selector: 'app-register',
   imports: [ReactiveFormsModule],
@@ -13,37 +13,45 @@ export class RegisterComponent {
 
 
 // form Control== Input
+RegisterForm = new FormGroup(
+  {
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(25),
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    rePassword: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    phone: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('^(010|011|012|015)\\d{8}$'),
+    ]),
+  },
+  this.ConfirmationPassword// Bind `this` to access the class context
+);
 
-  RegisterForm=new FormGroup({
-    name: new FormControl
-    (null,[Validators.required,Validators.minLength(3),Validators.maxLength(25)]),
-  
-  
-    email: new FormControl(null,[Validators.required,Validators.email]),
-   
-    password: new FormControl(null,[Validators.required,Validators.minLength(6)]),
-   
-    rePassword: new FormControl(null,[Validators.required,Validators.minLength(6)]),
-   
-    phone: new FormControl(null,[Validators.required,Validators.pattern('^(010|011|012|015)\d{8}$')]),
-  },this.ConfirmationPassword)
+// Custom Validation Function
+ConfirmationPassword(group: AbstractControl): ValidationErrors | null {
+  const password = group.get('password')?.value;
+  const rePassword = group.get('rePassword')?.value;
 
-
-
-ConfirmationPassword()
-{
-  const password = this.RegisterForm.get('password')?.value;
-  const rePassword = this.RegisterForm.get('rePassword')?.value;
-  if(password!==rePassword){
-   { confirmPasswordMismatch: true};
-
-  }else {
-   null
+  if (password !== rePassword) {
+    return { NotMatch: true };
   }
-
+  return null;
 }
 
 
-
+showData(event:any)
+{
+  console.log(event)
+}
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./Component/navbar/navbar.component";
+import { RegisterComponent } from "./Component/register/register.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
